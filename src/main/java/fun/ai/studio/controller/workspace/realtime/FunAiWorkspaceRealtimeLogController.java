cc4c4998617d9ca4d9a00dd8f63aade48f54af3b
jianrui <jianrui@fun.tv>
2026-02-09 15:16:19 +0800

    再次fixed bug

--- src/main/java/fun/ai/studio/controller/workspace/realtime/FunAiWorkspaceRealtimeLogController.java
+++ src/main/java/fun/ai/studio/controller/workspace/realtime/FunAiWorkspaceRealtimeLogController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.RandomAccessFile;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
